597b05c76b0a9b24c2a51f5b644ffdf1359489e2
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-07-04 20:21:01 +0100

    Added Application, Contact, CoverLetter, Resume Entities

--- backend/Database/ApplicationDbContext.cs
+++ backend/Database/ApplicationDbContext.cs
@@ -7,6 +7,10 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
     : IdentityDbContext<User, IdentityRole, string>(options)
 {
     public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<Application> Applications { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Resume> Resumes { get; set; }
+    public DbSet<CoverLetter> CoverLetters { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
