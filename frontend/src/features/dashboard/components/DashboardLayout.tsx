2efc58815ab90cadce8ee254ac5a6d00b0bdea5c
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-07-04 16:18:13 +0100

    Added Error Toasting

--- frontend/src/features/dashboard/components/DashboardLayout.tsx
+++ frontend/src/features/dashboard/components/DashboardLayout.tsx
@@ -4,11 +4,22 @@ import {
   DashboardSideBarLayout,
   DashboardHeaderLayout,
 } from "#/dashboard";
+import { useSearchParams } from "react-router-dom";
+import { toast } from "sonner";
+import { useEffect } from "react";
 
 export function DashboardLayout() {
   const { activeNavItemState } = useDashboardActiveNavItemStore();
   const ActiveComponent =
     dashboardSideBarNavigationComponentsConstant[activeNavItemState];
+  const [searchParams, setSearchParams] = useSearchParams();
+  useEffect(() => {
+    const message = searchParams.get("message");
+    if (message) {
+      toast.error(message);
+      setSearchParams({}, { replace: true });
+    }
+  }, [searchParams, setSearchParams]);
   return (
     <div className="bg-background flex h-screen">
       <DashboardSideBarLayout />
