5d162020ed28e7f040c097c3c1be227b2f2a930b
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-07-04 20:26:07 +0100

    Added Resume Entity

---
+++ backend/Entities/Resume.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Entities;
+
+public class Resume
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? Name { get; set; }
+    
+    public ICollection<Application> Applications { get; set; } = new List<Application>();
+}
