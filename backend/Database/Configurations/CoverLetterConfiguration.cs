02b822ea2eaab0a8a8e5267324811215afa905a2
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-07-04 20:23:54 +0100

    Added CoverLetter's Entity Configuration

---
+++ backend/Database/Configurations/CoverLetterConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using backend.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace backend.Database.Configuration;
+
+public class CoverLetterConfiguration : IEntityTypeConfiguration<CoverLetter>
+{
+    public void Configure(EntityTypeBuilder<CoverLetter> builder)
+    {
+        builder.HasKey(cl => cl.Id);
+        
+    }
+}
