51f1077b8933570f14d1152a953064e92db0b7a0
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-07-04 20:24:16 +0100

    Added Resume's Entity Configuration

---
+++ backend/Database/Configurations/ResumeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using backend.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace backend.Database.Configuration;
+
+public class ResumeConfiguration : IEntityTypeConfiguration<Resume>
+{
+    public void Configure(EntityTypeBuilder<Resume> builder)
+    {
+        builder.HasKey(r => r.Id);
+        
+        builder.HasMany(r => r.Applications)
+            .WithOne(a => a.ResumeUsed)
+            .HasForeignKey(a => a.ResumeId)
+            .IsRequired(false);
+    }
+}
