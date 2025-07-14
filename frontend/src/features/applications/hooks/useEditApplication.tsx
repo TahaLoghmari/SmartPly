6acf4b0def2845547013b00ee1dde1588e94e16b
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-07-14 12:22:54 +0100

    Refactor useEditApplication hook to accept id as a parameter and improve query invalidation logic

--- frontend/src/features/applications/hooks/useEditApplication.tsx
+++ frontend/src/features/applications/hooks/useEditApplication.tsx
@@ -2,16 +2,26 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { type ApplicationRequestDto, editApplication } from "#/applications";
 import { useCurrentUser } from "#/auth";
 
-export function useEditApplication({ id }: { id: string }) {
+export function useEditApplication() {
   const queryClient = useQueryClient();
   const { data: user } = useCurrentUser();
   return useMutation({
-    mutationFn: (credentials: ApplicationRequestDto) =>
-      editApplication(id, credentials),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["application", id] });
+    mutationFn: ({
+      id,
+      credentials,
+    }: {
+      id: string;
+      credentials: ApplicationRequestDto;
+    }) => editApplication(id, credentials),
+    onSuccess: (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ["application", variables.id],
+      });
       queryClient.invalidateQueries({
         queryKey: ["applications", user?.id],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["applicationStats", user?.id],
       });
     },
   });
