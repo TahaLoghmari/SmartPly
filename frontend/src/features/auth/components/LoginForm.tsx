9ebb30013b12de5d5b8e76d14424c5d4b88a7150
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-07-20 15:24:07 +0100

    refactor(auth): simplify RegisterPage and add ForgotPasswordPage
    - Removed Google OAuth button and related logic from RegisterPage.
    - Introduced ForgotPasswordPage component with ForgotPasswordForm.
    - Added form validation schemas for registration and login using Zod.
    - Created a new Card component for better UI structure.
    - Updated Button component for improved styling consistency.
    - Cleaned up unused imports and optimized existing code.

--- frontend/src/features/auth/components/LoginForm.tsx
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -1,38 +1,44 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Link } from "react-router-dom";
+import {
+  useGetGoogleOAuthUrl,
+  useLogin,
+  type LoginUserDto,
+  loginFormSchema,
+} from "#/auth";
+import { Link, useSearchParams } from "react-router-dom";
+import { toast } from "sonner";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { useLogin, type LoginUserDto } from "#/auth";
-
-import { Spinner } from "@/components/ui/spinner";
-
-const formSchema = z.object({
-  email: z
-    .string()
-    .min(1, { message: "Email is required." })
-    .email({ message: "Invalid email format." })
-    .max(256, { message: "Email must be at most 256 characters." }),
-  password: z.string().min(8, {
-    message: "Password must be at least 8 characters.",
-  }),
-});
-
-export function LoginForm() {
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const getGoogleOAuthUrlMutation = useGetGoogleOAuthUrl();
+  const [searchParams, setSearchParams] = useSearchParams();
   const loginMutation = useLogin();
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
     mode: "onChange",
     defaultValues: {
       email: "",
@@ -45,80 +51,137 @@ export function LoginForm() {
   const isEmailNotVerified =
     loginMutation.error?.message?.startsWith("Email not Verified");
   const errorMessage = loginMutation.error?.message;
+  useEffect(() => {
+    const message = searchParams.get("message");
+    if (message) {
+      toast.error(message);
+      setSearchParams({}, { replace: true });
+    }
+  }, [searchParams, setSearchParams]);
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
-        {loginMutation.isError && (
-          <div className="bg-destructive/10 border-destructive text-destructive mb-4 rounded-md border p-3 text-sm">
-            <div>{errorMessage}</div>
-            {isEmailNotVerified && (
-              <div className="mt-2">
-                <Link
-                  to={`/email-verification/?email=${form.getValues("email")}`}
-                  className="text-muted-foreground hover:text-muted-foreground/20 underline"
-                >
-                  Resend Email
-                </Link>
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardDescription>Login with your Google account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              {loginMutation.isError && (
+                <div className="bg-destructive/10 border-destructive text-destructive mb-4 rounded-md border p-3 text-sm">
+                  <div>{errorMessage}</div>
+                  {isEmailNotVerified && (
+                    <div className="mt-2">
+                      <Link
+                        to={`/email-verification/?email=${form.getValues("email")}`}
+                        className="text-muted-foreground hover:text-muted-foreground/20 underline"
+                      >
+                        Resend Email
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              )}
+              <div className="grid gap-6">
+                <div className="flex flex-col gap-4">
+                  <Button
+                    variant="outline"
+                    className="w-full"
+                    onClick={() => {
+                      getGoogleOAuthUrlMutation.mutate();
+                    }}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                      <path
+                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                    Login with Google
+                  </Button>
+                </div>
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-card text-muted-foreground relative z-10 px-2">
+                    Or continue with
+                  </span>
+                </div>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="email"
+                              placeholder="Email"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <div className="flex items-center">
+                            <FormLabel>Password</FormLabel>
+                            <Link
+                              to="/forgot-password"
+                              className="ml-auto text-sm underline-offset-4 hover:underline"
+                            >
+                              Forgot your password?
+                            </Link>
+                          </div>
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder="Password"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <Button
+                    type="submit"
+                    className="w-full"
+                    disabled={
+                      !form.formState.isValid || loginMutation.isPending
+                    }
+                  >
+                    {loginMutation.isPending ? (
+                      <Spinner className="h-8 w-auto invert dark:invert-0" />
+                    ) : (
+                      "Login"
+                    )}
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <Link to="/register" className="underline underline-offset-4">
+                    Sign up
+                  </Link>
+                </div>
               </div>
-            )}
-          </div>
-        )}
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" placeholder="Email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="Password" {...field} />
-              </FormControl>
-              <FormDescription>
-                <Link
-                  to="/forgot-password"
-                  className="text-muted-foreground font-semibold underline decoration-solid"
-                >
-                  Forgot your password?
-                </Link>
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <Button
-          type="submit"
-          className="w-full cursor-pointer"
-          disabled={!form.formState.isValid || loginMutation.isPending}
-        >
-          {loginMutation.isPending ? (
-            <Spinner className="h-8 w-auto invert dark:invert-0" />
-          ) : (
-            "Login"
-          )}
-        </Button>
-        <p className="text-sm font-semibold">
-          Don't have an account ?{" "}
-          <Link
-            to="/register"
-            className="text-muted-foreground font-semibold underline decoration-solid"
-          >
-            Register now
-          </Link>
-        </p>
-      </form>
-    </Form>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
   );
 }
--- frontend/src/features/auth/components/RegisterForm.tsx
+++ frontend/src/features/auth/components/RegisterForm.tsx
@@ -1,7 +1,7 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
@@ -10,57 +10,34 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import {
+  useGetGoogleOAuthUrl,
+  useRegister,
+  type RegisterUserDto,
+  registerFormSchema,
+} from "#/auth";
 import { Link } from "react-router-dom";
-
-import { useRegister, type RegisterUserDto } from "#/auth";
-import { Spinner } from "@/components/ui/spinner";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { z } from "zod";
 
-const formSchema = z
-  .object({
-    name: z
-      .string()
-      .min(1, { message: "Name is required." })
-      .max(50, { message: "Name must be at most 50 characters." }),
-    email: z
-      .string()
-      .min(1, { message: "Email is required." })
-      .email({ message: "Invalid email format." })
-      .max(256, { message: "Email must be at most 256 characters." }),
-    password: z
-      .string()
-      .min(1, { message: "Password is required." })
-      .min(6, { message: "Password must be at least 6 characters long." })
-      .max(100, { message: "Password must be at most 100 characters long." })
-      .regex(/[A-Z]/, {
-        message: "Password must contain at least one uppercase letter (A-Z).",
-      })
-      .regex(/[a-z]/, {
-        message: "Password must contain at least one lowercase letter (a-z).",
-      })
-      .regex(/[0-9]/, {
-        message: "Password must contain at least one digit (0-9).",
-      })
-      .regex(/[^a-zA-Z0-9]/, {
-        message:
-          "Password must contain at least one non-alphanumeric character.",
-      }),
-    confirmPassword: z
-      .string()
-      .min(1, { message: "Confirmation password is required." }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Password and confirmation password do not match.",
-    path: ["confirmPassword"],
-  });
-
-export function RegisterForm() {
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const getGoogleOAuthUrlMutation = useGetGoogleOAuthUrl();
   const registerMutation = useRegister();
   const navigate = useNavigate();
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof registerFormSchema>>({
+    resolver: zodResolver(registerFormSchema),
     mode: "onChange",
     defaultValues: {
       email: "",
@@ -77,90 +54,145 @@ export function RegisterForm() {
     });
   };
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
-        {registerMutation.isError && (
-          <div className="bg-destructive/10 border-destructive text-destructive mb-4 rounded-md border p-3 text-sm">
-            {registerMutation.error.message}
-          </div>
-        )}
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Name" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" placeholder="Email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="Password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="confirmPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirm Password</FormLabel>
-              <FormControl>
-                <Input
-                  type="password"
-                  placeholder="Confirm Password"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button
-          type="submit"
-          className="w-full cursor-pointer"
-          disabled={!form.formState.isValid || registerMutation.isPending}
-        >
-          {registerMutation.isPending ? (
-            <Spinner className="h-8 w-auto invert dark:invert-0" />
-          ) : (
-            "Sign up"
-          )}
-        </Button>
-        <p className="text-sm font-semibold">
-          Already have an account ?{" "}
-          <Link
-            to="/login"
-            className="text-muted-foreground font-semibold underline decoration-solid"
-          >
-            Login
-          </Link>
-        </p>
-      </form>
-    </Form>
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardDescription>Login with your Google account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              {registerMutation.isError && (
+                <div className="bg-destructive/10 border-destructive text-destructive mb-4 rounded-md border p-3 text-sm">
+                  {registerMutation.error.message}
+                </div>
+              )}
+              <div className="grid gap-6">
+                <div className="flex flex-col gap-4">
+                  <Button
+                    variant="outline"
+                    className="w-full"
+                    onClick={() => {
+                      getGoogleOAuthUrlMutation.mutate();
+                    }}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                      <path
+                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                    Login with Google
+                  </Button>
+                </div>
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-card text-muted-foreground relative z-10 px-2">
+                    Or continue with
+                  </span>
+                </div>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Name</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Name" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="email"
+                              placeholder="Email"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder="Password"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="confirmPassword"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Confirm Password</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder="Confirm Password"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <Button
+                    type="submit"
+                    className="w-full"
+                    disabled={
+                      !form.formState.isValid || registerMutation.isPending
+                    }
+                  >
+                    {registerMutation.isPending ? (
+                      <Spinner className="h-8 w-auto invert dark:invert-0" />
+                    ) : (
+                      "Sign up"
+                    )}
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Have an account?{" "}
+                  <Link to="/login" className="underline underline-offset-4">
+                    Sign in
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
   );
 }
