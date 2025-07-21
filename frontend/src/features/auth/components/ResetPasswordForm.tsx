bba94228a5cbb8a8b52f7699763732ee212cf032
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-07-21 08:30:39 +0100

    - Updated ResetPasswordForm component to use a new resetPasswordFormSchema for form validation. - Enhanced UI by wrapping the form in a Card component for better presentation. - Removed unnecessary testForm and testPage components. - Updated ResetPasswordPage to include a new SVG logo and improved layout. - Added resetPasswordFormSchema to types.ts for centralized validation schema management.

--- frontend/src/features/auth/components/ResetPasswordForm.tsx
+++ frontend/src/features/auth/components/ResetPasswordForm.tsx
@@ -13,41 +13,18 @@ import {
 import { Input } from "@/components/ui/input";
 import { Spinner } from "@/components/ui/spinner";
 import { useSearchParams } from "react-router-dom";
-import { useResetPassword, type ResetPasswordDto } from "#/auth";
-
-const formSchema = z
-  .object({
-    email: z
-      .string()
-      .min(1, { message: "Email is required." })
-      .email({ message: "Invalid email format." })
-      .max(256, { message: "Email must be at most 256 characters." }),
-    password: z
-      .string()
-      .min(1, { message: "Password is required." })
-      .min(6, { message: "Password must be at least 6 characters long." })
-      .max(100, { message: "Password must be at most 100 characters long." })
-      .regex(/[A-Z]/, {
-        message: "Password must contain at least one uppercase letter (A-Z).",
-      })
-      .regex(/[a-z]/, {
-        message: "Password must contain at least one lowercase letter (a-z).",
-      })
-      .regex(/[0-9]/, {
-        message: "Password must contain at least one digit (0-9).",
-      })
-      .regex(/[^a-zA-Z0-9]/, {
-        message:
-          "Password must contain at least one non-alphanumeric character.",
-      }),
-    confirmPassword: z
-      .string()
-      .min(1, { message: "Confirmation password is required." }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Password and confirmation password do not match.",
-    path: ["confirmPassword"],
-  });
+import {
+  useResetPassword,
+  type ResetPasswordDto,
+  resetPasswordFormSchema,
+} from "#/auth";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export function ResetPasswordForm() {
   const resetPasswordMutation = useResetPassword();
@@ -55,8 +32,8 @@ export function ResetPasswordForm() {
   const token = searchParams.get("token");
   const email = searchParams.get("email");
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof resetPasswordFormSchema>>({
+    resolver: zodResolver(resetPasswordFormSchema),
     mode: "onChange",
     defaultValues: {
       email: email || "",
@@ -64,7 +41,7 @@ export function ResetPasswordForm() {
       confirmPassword: "",
     },
   });
-  async function onSubmit(formData: z.infer<typeof formSchema>) {
+  async function onSubmit(formData: z.infer<typeof resetPasswordFormSchema>) {
     const dto: ResetPasswordDto = {
       email: formData.email,
       newPassword: formData.password,
@@ -74,68 +51,92 @@ export function ResetPasswordForm() {
     resetPasswordMutation.mutate(dto);
   }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
-        {resetPasswordMutation.isError && (
-          <div className="bg-destructive/10 border-destructive text-destructive mb-4 rounded-md border p-3 text-sm">
-            {resetPasswordMutation.error.message}
-          </div>
-        )}
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" placeholder="Email" readOnly {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="Password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="confirmPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirm Password</FormLabel>
-              <FormControl>
-                <Input
-                  type="password"
-                  placeholder="Confirm Password"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button
-          type="submit"
-          className="w-full cursor-pointer"
-          disabled={!form.formState.isValid || resetPasswordMutation.isPending}
-        >
-          {resetPasswordMutation.isPending ? (
-            <Spinner className="h-8 w-auto invert dark:invert-0" />
-          ) : (
-            "Reset Password"
-          )}
-        </Button>
-      </form>
-    </Form>
+    <div className="flex flex-col gap-6">
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Reset your Password</CardTitle>
+          <CardDescription>Enter your new password</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full space-y-6"
+            >
+              {resetPasswordMutation.isError && (
+                <div className="bg-destructive/10 border-destructive text-destructive mb-4 rounded-md border p-3 text-sm">
+                  {resetPasswordMutation.error.message}
+                </div>
+              )}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="Email"
+                        readOnly
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Confirm Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="w-full cursor-pointer"
+                disabled={
+                  !form.formState.isValid || resetPasswordMutation.isPending
+                }
+              >
+                {resetPasswordMutation.isPending ? (
+                  <Spinner className="h-8 w-auto invert dark:invert-0" />
+                ) : (
+                  "Reset Password"
+                )}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
