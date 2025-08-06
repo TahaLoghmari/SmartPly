1197a4ca1241de01854dfd605ca30dc3c86bf5b3
Mohamed Taha Loghmari <medtahalog@gmail.com>
2025-08-06 15:34:33 +0100

    Added infinite scroll fetching with pagination

--- frontend/src/features/inbox/components/Inbox.tsx
+++ frontend/src/features/inbox/components/Inbox.tsx
@@ -1,22 +1,26 @@
 import { MailCard, useGetUserEmails, type Email } from "#/inbox";
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
+import { Spinner } from "@/components/ui/spinner";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 export function Inbox() {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
     useGetUserEmails();
 
-  const emails = data?.pages.flatMap((page) => page.messages) ?? [];
+  const { ref, inView } = useInView({ rootMargin: "200px" });
 
-  console.log("data: ", data?.pages);
-  console.log("email: ", emails);
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+  const emails = data?.pages.flatMap((page) => page.messages) ?? [];
 
   if (isLoading)
     return (
-      <div className="flex flex-col items-center gap-2">
-        {Array.from({ length: 8 }).map((_, index) => (
-          <Skeleton key={index} className="h-[99px] w-[95%]" />
-        ))}
+      <div className="flex h-[80svh] flex-1 flex-col items-center justify-center gap-2">
+        <Spinner className="dark:invert" />
       </div>
     );
 
@@ -26,11 +30,12 @@ export function Inbox() {
         <MailCard email={email} key={email.id} />
       ))}
 
-      {hasNextPage && (
-        <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
-          {isFetchingNextPage ? "Loading more..." : "Load more"}
-        </Button>
+      {isFetchingNextPage && (
+        <div className="mt-6 flex w-full flex-1 flex-col items-center justify-center">
+          <Spinner className="dark:invert" />
+        </div>
       )}
+      {hasNextPage && <div ref={ref} />}
     </>
   );
 }
